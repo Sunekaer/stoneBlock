17f8d785e64ac93ee03c74a528fe2fb2e0e9c9e7
knoxhack <hackofgod@gmail.com>
2018-10-20 05:52:16 -0400

    Added Server Files/Scripts

--- Install.bat
+++ Install.bat
@@ -1,1 +1,1 @@
-curl -o minecraft_server.jar https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar
+curl -o minecraft_server.1.12.2.jar https://launcher.mojang.com/v1/objects/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar
---
+++ LaunchServer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xmx4G -XX:MaxPermSize=256M -jar forge-1.12.2-14.23.5.2768-universal.jar nogui
